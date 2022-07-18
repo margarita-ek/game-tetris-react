f0d727b970cc601f03320b831cbcd5329dbf0000
Margo <ekimargarita@gmail.com>
2022-07-18 13:38:46 +0300

    start dev

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './App';
+import './style/style.css'
+
+ReactDOM.render(
+  <div className='wrapper'>
+    <App />
+  </div>,
+  document.getElementById('root')
+);
